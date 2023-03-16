fd73157761520edf1c1f2df72f65847dac4ea203
juanjo <juanjo@nextail.co>
2023-03-16 19:14:41 +0100

    Feat: Render a list of Guess components

--- src/components/Guesses/Guesses.js
+++ src/components/Guesses/Guesses.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import Guess from './../Guess';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+import { range } from '../../utils';
 
 function Guesses({ guesses }) {
   return (
     <div className="guess-results">
-      {guesses.map((guess, idx) => (
-        <p key={`${idx}-${guess}`} className="guess">
-          {guess}
-        </p>
+      {range(NUM_OF_GUESSES_ALLOWED).map((row, rowIdx) => (
+        <Guess key={rowIdx} word={guesses[rowIdx]} />
       ))}
     </div>
   );
