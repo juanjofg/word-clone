c4adb0cc58d88d84bfbbb8126cbbfbec8e26f754
juanjo <juanjo@nextail.co>
2023-03-20 19:34:50 +0100

    Feat: Display Banner in Game component

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Guesses from './../Guesses';
 import GuessInput from './../GuessInput';
+import Banner from '../Banner/Banner';
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -11,17 +13,30 @@ console.info({ answer });
 
 function Game() {
   const [guesses, setGuesses] = React.useState([]);
+  const [showBanner, setShowBanner] = React.useState(false);
+  const [type, setType] = React.useState('fail');
 
   const handleInput = (value) => {
     const nextGuesses = [...guesses];
     nextGuesses.push(value);
+    if (value === answer) {
+      setType('success');
+      setShowBanner(true);
+    } else if (nextGuesses.length >= NUM_OF_GUESSES_ALLOWED) {
+      setType('fail');
+      setShowBanner(true);
+    }
+
     setGuesses(nextGuesses);
   };
 
   return (
     <>
       <Guesses guesses={guesses} answer={answer} />
-      <GuessInput handleInput={handleInput} />
+      <GuessInput handleInput={handleInput} disabled={showBanner} />
+      {showBanner && (
+        <Banner attempts={guesses.length} answer={answer} type={type} />
+      )}
     </>
   );
 }
